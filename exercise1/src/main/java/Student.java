36ca0640e26833450bee1741202a3c417897e906
darthkojones <darthkojones@gmail.com>
2024-10-23 19:11:58 +0200

    .

---
+++ exercise1/src/main/java/Student.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author darth
+ */
+public class Student {
+    
+    
+    
+    
+    public String printStudentInfo() {
+        String res = "Student name: " + this.name + "/n" 
+                + "Age: " +this.age }
+}
