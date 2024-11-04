e1d33bc081281185bcf6533b9e83fd778bd1108f
darthkojones <darthkojones@gmail.com>
2024-11-04 08:48:40 +0100

    a bit of initial work in terms of planning for ex 3.2

---
+++ exercise3/src/main/java/at/mci/adrianpetre/drinkDispenser/Drink.java
@@ -0,0 +1,18 @@
+package at.mci.adrianpetre.drinkDispenser;
+
+
+/*
+ * this will be an interface that can be adapted to the various types of drinks
+ * Tea, Coffee, Water
+ */
+public interface Drink {
+
+
+
+
+    void dispense ();
+        //common method for all drinks to be dispensed
+        //it will be called upon and overridden
+
+    
+}
---
+++ exercise3/src/main/java/at/mci/adrianpetre/drinkDispenser/Tea.java
@@ -0,0 +1,5 @@
+package at.mci.adrianpetre.drinkDispenser;
+
+public class Tea {
+
+}
